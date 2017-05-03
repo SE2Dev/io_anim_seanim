75264c84ed3c1ff8a0c700311c78c3baeb841576
SE2Dev <brokensilencedev@gmail.com>
2017-05-03 17:08:27 -0400

    Bump Version from 0.3.2 -> 0.3.3

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ import time
 bl_info = {
     "name": "SEAnim Support",
     "author": "SE2Dev",
-    "version": (0, 3, 2),
+    "version": (0, 3, 3),
     "blender": (2, 78, 0),
     "location": "File > Import",
     "description": "Import SEAnim",
